c1572d1a0fe2d72a135adc243e59b92c6ae42ac7
jackSharp2 <105824982+jackSharp2@users.noreply.github.com>
2022-12-26 10:19:17 -0700

    Rarities and drops
    started drops did particles for all rarities of item

--- My project/Assets/Scripts/ChestScripts/ChestCollision.cs
+++ My project/Assets/Scripts/ChestScripts/ChestCollision.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class ChestCollision : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameObject Chest;
+    GameObject chest;
+    GeneratIteams iteams;
     void Start()
     {
-        Chest = GameObject.Find("ResorseChests");
+        chest = GameObject.Find("ResorseChests");
+        iteams = chest.GetComponent<GeneratIteams>();
     }
 
     // Update is called once per frame
@@ -24,7 +26,8 @@ public class ChestCollision : MonoBehaviour
 
         if (tag == "Player")
         {
-            Destroy(Chest);
+            Destroy(chest);
+            iteams.getDrops(chest);
         }
     }
 }
--- My project/Assets/Scripts/ChestScripts/GeneratIteams.cs
+++ My project/Assets/Scripts/ChestScripts/GeneratIteams.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class GeneratIteams : MonoBehaviour
 {
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void getDrops(GameObject chest) 
+    {
+    
     }
 }
